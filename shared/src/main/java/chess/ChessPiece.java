b58d62c6457e2b1de76fa21f05d35a02cae5e4e7
Seth <seth.hilton@gmail.com>
2026-05-01 18:13:46 -0600

    phase 0 - implementing the object overrides for the equals() and hashCode() methods in ChessPosition, ChessMove, ChessBoard, & ChessPiece

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -3,6 +3,7 @@ package chess;
 import java.util.Collection;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -44,6 +45,20 @@ public class ChessPiece {
      */
     public PieceType getPieceType() {
         return type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
     }
 
     /**
@@ -292,7 +307,6 @@ public class ChessPiece {
         }
 
         return moves;
-//        List.of(new ChessMove(new ChessPosition(5,4), new ChessPosition(1,8), null));
     }
 
     public Collection<ChessMove> rookMoves(ChessBoard board, ChessPosition myPosition) {
