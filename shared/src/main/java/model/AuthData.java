3d9cb6156016e00477a14813e5c893a96749c1b5
Seth <seth.hilton@gmail.com>
2026-05-10 00:35:09 -0600

    phase 3 - implementing UserData, GameData, and AuthData Data Model classes

---
+++ shared/src/main/java/model/AuthData.java
@@ -0,0 +1,18 @@
+package model;
+
+public class AuthData {
+    private String username;
+    private String authToken;
+    public AuthData(String username, String authToken) {
+        this.username = username;
+        this.authToken = authToken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+}
