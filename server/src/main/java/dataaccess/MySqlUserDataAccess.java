6c4b5c617859e6defffcc0771aedb32cb991063f
Seth <seth.hilton@gmail.com>
2026-05-26 13:04:27 -0600

    phase 4 - fixes to create table initialization in MySqlUserDataAccess

--- server/src/main/java/dataaccess/MySqlUserDataAccess.java
+++ server/src/main/java/dataaccess/MySqlUserDataAccess.java
@@ -10,6 +10,7 @@ import static java.sql.Statement.RETURN_GENERATED_KEYS;
 public class MySqlUserDataAccess implements UserDataAccess {
     public MySqlUserDataAccess() {
         try {
+//            System.out.println("about to configure the database...");
             configureDatabase();
         } catch (DataAccessException e) {
             throw new RuntimeException(e);
@@ -24,7 +25,7 @@ public class MySqlUserDataAccess implements UserDataAccess {
 
     public UserData getUser(String username) throws DataAccessException, SQLException {
         try (var conn = DatabaseManager.getConnection()) {
-            var statement = "SELECT username, json FROM pet WHERE username=?";
+            var statement = "SELECT username, json FROM users WHERE username=?";
             try (var ps = conn.prepareStatement(statement)) {
                 ps.setString(1, username);
                 try (var rs = ps.executeQuery()) {
@@ -48,12 +49,15 @@ public class MySqlUserDataAccess implements UserDataAccess {
 
     private UserData readUser(ResultSet rs) throws SQLException {
         var username = rs.getString("username");
-        var json = rs.getString("json");
-        var user = new Gson().fromJson(json, UserData.class);
-        return user;
+        var password = rs.getString("password");
+        var email = rs.getString("email");
+//        var json = rs.getString("json");
+//        var user = new Gson().fromJson(json, UserData.class);
+        return new UserData(username, password, email);
     }
 
     private void executeUpdate(String statement, Object... params) throws DataAccessException, SQLException {
+//        System.out.println("in executeUpdate...");
         try (var conn = DatabaseManager.getConnection()) {
             try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
                 for (var i = 0; i < params.length; i++) {
@@ -69,24 +73,32 @@ public class MySqlUserDataAccess implements UserDataAccess {
 
     private final String[] createStatements = {
             """
-            CREATE TABLE IF NOT EXISTS user (
-            'username' varchar(256) NOT NULL,
-            'password' varchar(256) NOT NULL,
-            'email' varchar(256) NOT NULL,
-            PRIMARY KEY (username)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=ut8mb4_0900_ai_ci
+            CREATE TABLE IF NOT EXISTS users (
+                username varchar(256) NOT NULL,
+                password varchar(256) NOT NULL,
+                email varchar(256) NOT NULL,
+                PRIMARY KEY(username)
+            ) 
             """
     };
 
     private void configureDatabase() throws DataAccessException {
+//        System.out.println("about to create the database...");
         DatabaseManager.createDatabase();
+//        System.out.println("Created the database...");
         try (var conn = DatabaseManager.getConnection()) {
+//            System.out.println("Got the database connection...");
             for (var statement : createStatements) {
                 try (var preparedStatement = conn.prepareStatement(statement)) {
+//                    System.out.print("executing the statement: ");
+//                    System.out.println(statement);
                     preparedStatement.executeUpdate();
+//                    System.out.println("executed the statement...");
                 }
             }
+//            System.out.println("Created all the statements...");
         } catch (SQLException ex) {
+//            System.out.println(ex.getMessage());
             throw new DataAccessException();
         }
     }
