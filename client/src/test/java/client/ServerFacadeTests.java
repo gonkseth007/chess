52abb0af695ec05b5ee352dba2b174a005e30299
Seth <seth.hilton@gmail.com>
2026-05-31 00:28:17 -0600

    phase 5 - implementing positive and negative tests for register, login, logout, and clear for ServerFacade

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,6 +1,7 @@
 package client;
 
-import model.RegisterRequest;
+import dataaccess.*;
+import model.*;
 import org.junit.jupiter.api.*;
 import server.Server;
 
@@ -32,10 +33,72 @@ public class ServerFacadeTests {
 
     @Test
     @DisplayName("Successfully Register User")
-    public void registerUserSuccess() throws ResponseException {
+    void registerUserSuccess() throws ResponseException {
         var result = facade.register(new RegisterRequest("gonkdroid007", "gonkdroidrules", "gonk@gonk.gonk"));
         assertEquals("gonkdroid007", result.username());
         assertTrue(result.authToken().length() > 10);
+    }
+
+    @Test
+    @DisplayName("Fail to Register User")
+    void registerUserFail() throws ResponseException {
+        facade.register(new RegisterRequest("gonkdroid007", "gonkdroidrules", "gonk@gonk.gonk"));
+        assertThrows(ResponseException.class, () -> facade.register(new RegisterRequest("gonkdroid007", "iforgotoldpasswordsore-registering", "gonk@gonk.droid")));
+        assertThrows(ResponseException.class, () -> facade.register(new RegisterRequest(null, "nouserhahaimsoclever", "troll@trolls.edu")));
+    }
+
+    @Test
+    @DisplayName("Successfully Login User")
+    void loginUserSuccess() throws ResponseException {
+        facade.register(new RegisterRequest("gonkdroid007", "gonkdroidrules", "gonk@gonk.gonk"));
+        RegisterLoginResult result = facade.login(new LoginRequest("gonkdroid007", "gonkdroidrules"));
+        assertEquals("gonkdroid007", result.username());
+        assertTrue(result.authToken().length() > 10);
+    }
+
+    @Test
+    @DisplayName("Fail to Login User")
+    void loginUserFail() throws ResponseException {
+        facade.register(new RegisterRequest("gonkdroid007", "gonkdroidrules", "gonk@gonk.gonk"));
+        assertThrows(AuthorizationException.class, () -> facade.login(new LoginRequest("gonkdroid007", "iknowmypasswordtrust")));
+        assertThrows(BadRequestException.class, () -> facade.login(new LoginRequest(null, "lamepassword")));
+    }
+
+    @Test
+    @DisplayName("Successfully Logout User")
+    void logoutUserSuccess() throws ResponseException, DataAccessException {
+        MySqlUserDataAccess users = new MySqlUserDataAccess();
+        facade.register(new RegisterRequest("gonkdroid007", "gonkdroidrules", "gonk@gonk.gonk"));
+        RegisterLoginResult result = facade.login(new LoginRequest("gonkdroid007", "starwarsiscool"));
+        assertEquals("gonkdroid007", result.username());
+        facade.logout(result.authToken());
+        assertNull(users.getUser("gonkdroid007"));
+    }
+
+    @Test
+    @DisplayName("Fail to Logout User")
+    void logoutUserFail() throws ResponseException {
+        facade.register(new RegisterRequest("gonkdroid007", "gonkdroidrules", "gonk@gonk.gonk"));
+        assertThrows(AuthorizationException.class, () -> facade.logout("thisistotallyarealAuthToken"));
+        RegisterLoginResult result = facade.login(new LoginRequest("gonkdroid007", "starwarsiscool"));
+        assertThrows(AuthorizationException.class, () -> facade.logout(String.format("someextra%s", result.authToken())));
+    }
+
+    @Test
+    @DisplayName("Successful Clear Database")
+    void clearDatabase() throws ResponseException, DataAccessException {
+        var userDAO = new MySqlUserDataAccess();
+        var authDAO = new MySqlAuthDataAccess();
+        var gameDAO = new MySqlGameDataAccess();
+        userDAO.insertUser(new UserData("gonkdroid007", "starwarsiscool", "gonk@gonk.edu"));
+        authDAO.insertAuth(new AuthData("gonkdroid007", "12345"));
+        gameDAO.createGame("Gonk's Game");
+        gameDAO.createGame("James Bond's Game");
+        gameDAO.createGame("Chess Battle");
+        facade.clearDatabase();
+        assertNull(userDAO.getUser("gonkdroid007"));
+        assertNull(authDAO.getAuth("12345"));
+        assertEquals(0, gameDAO.getAllGames().size());
     }
 
 }
