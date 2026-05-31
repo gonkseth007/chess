01ef7167e13d03b65f0111de6c217ab4c676328f
Seth <seth.hilton@gmail.com>
2026-05-31 00:32:49 -0600

    phase 5 - implementing positive and negative tests for createGame, joinGame, and listGames functions in ServerFacade

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -60,8 +60,8 @@ public class ServerFacadeTests {
     @DisplayName("Fail to Login User")
     void loginUserFail() throws ResponseException {
         facade.register(new RegisterRequest("gonkdroid007", "gonkdroidrules", "gonk@gonk.gonk"));
-        assertThrows(AuthorizationException.class, () -> facade.login(new LoginRequest("gonkdroid007", "iknowmypasswordtrust")));
-        assertThrows(BadRequestException.class, () -> facade.login(new LoginRequest(null, "lamepassword")));
+        assertThrows(ResponseException.class, () -> facade.login(new LoginRequest("gonkdroid007", "iknowmypasswordtrust")));
+        assertThrows(ResponseException.class, () -> facade.login(new LoginRequest(null, "lamepassword")));
     }
 
     @Test
@@ -79,9 +79,65 @@ public class ServerFacadeTests {
     @DisplayName("Fail to Logout User")
     void logoutUserFail() throws ResponseException {
         facade.register(new RegisterRequest("gonkdroid007", "gonkdroidrules", "gonk@gonk.gonk"));
-        assertThrows(AuthorizationException.class, () -> facade.logout("thisistotallyarealAuthToken"));
+        assertThrows(ResponseException.class, () -> facade.logout("thisistotallyarealAuthToken"));
         RegisterLoginResult result = facade.login(new LoginRequest("gonkdroid007", "starwarsiscool"));
-        assertThrows(AuthorizationException.class, () -> facade.logout(String.format("someextra%s", result.authToken())));
+        assertThrows(ResponseException.class, () -> facade.logout(String.format("someextra%s", result.authToken())));
+    }
+
+    @Test
+    @DisplayName("Successfully Create Game")
+    void createGameSuccess() throws DataAccessException {
+        var gameDAO = new MySqlGameDataAccess();
+        CreateGameResult result = facade.createGame(new CreateGameRequest("Cool Game", "auth1234"));
+        assertEquals(1, result.gameID());
+        assertEquals("Cool Game", gameDAO.getGame(result.gameID()).gameName());
+        assertNull(gameDAO.getGame(result.gameID()).whiteUsername());
+        assertNull(gameDAO.getGame(result.gameID()).blackUsername());
+    }
+
+    @Test
+    @DisplayName("Fail to Create Game")
+    void createGameFail() {
+        assertThrows(ResponseException.class, () -> facade.createGame(new CreateGameRequest("COOL GAME", "fakeAuth123")));
+        assertThrows(ResponseException.class, () -> facade.createGame(new CreateGameRequest(null, "auth1234")));
+    }
+
+    @Test
+    @DisplayName("Successfully Join Game")
+    void joinGameSuccess() throws DataAccessException {
+        var gameDAO = new MySqlGameDataAccess();
+        CreateGameResult result = facade.createGame(new CreateGameRequest("Cool Game", "auth1234"));
+        facade.joinGame(new JoinGameRequest("WHITE", result.gameID(), "auth1234"));
+        assertEquals("gonkgonk", gameDAO.getGame(result.gameID()).whiteUsername());
+        assertNull(gameDAO.getGame(result.gameID()).blackUsername());
+    }
+
+    @Test
+    @DisplayName("Fail to Join Game")
+    void joinGameFail() throws ResponseException {
+        CreateGameResult result = facade.createGame(new CreateGameRequest("Cool Game", "auth1234"));
+        facade.joinGame(new JoinGameRequest("WHITE", result.gameID(), "auth5678"));
+        assertThrows(ResponseException.class, () -> facade.joinGame(new JoinGameRequest("BLACK", result.gameID(), "fake_auth_hehehe")));
+        assertThrows(ResponseException.class, () -> facade.joinGame(new JoinGameRequest("GRAY", result.gameID(), "auth1234")));
+        assertThrows(ResponseException.class, () -> facade.joinGame(new JoinGameRequest("WHITE", result.gameID(), "auth1234")));
+    }
+
+    @Test
+    @DisplayName("Successfully List Game")
+    void listGamesSuccess() {
+        CreateGameResult game1 = facade.createGame(new CreateGameRequest("Cool Game", "auth1234"));
+        CreateGameResult game2 = facade.createGame(new CreateGameRequest("Lame Game", "auth5678"));
+
+        ListGamesResult games = facade.listGames("auth1234");
+        assertEquals("Cool Game", games.games().getFirst().gameName());
+        assertEquals(2, games.games().get(1).gameID());
+        assertNotEquals(game1, game2);
+    }
+
+    @Test
+    @DisplayName("Fail to List Game")
+    void listGamesFail() {
+        assertThrows(ResponseException.class, () -> facade.listGames("fake_auth_hehehe"));
     }
 
     @Test
