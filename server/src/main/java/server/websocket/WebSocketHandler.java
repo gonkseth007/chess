e66b8efbc6d8bf552f9cde9961b5205fee338c9f
Seth <seth.hilton@gmail.com>
2026-06-07 01:16:14 -0600

    phase 6 - implementing skeleton code for WebSocketHandler class

---
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -0,0 +1,72 @@
+package server.websocket;
+
+//import exception.ResponseException;
+import com.google.gson.Gson;
+import io.javalin.websocket.WsCloseContext;
+import io.javalin.websocket.WsCloseHandler;
+import io.javalin.websocket.WsConnectContext;
+import io.javalin.websocket.WsConnectHandler;
+import io.javalin.websocket.WsMessageContext;
+import io.javalin.websocket.WsMessageHandler;
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.commands.UserGameCommand;
+//import webSocketMessages.Action;
+//import client.ResponseException;
+
+import java.io.IOException;
+
+import static websocket.commands.UserGameCommand.CommandType.*;
+
+public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
+
+    private final ConnectionManager connections = new ConnectionManager();
+
+    @Override
+    public void handleConnect(WsConnectContext ctx) {
+        System.out.println("Websocket connected");
+        ctx.enableAutomaticPings();
+    }
+
+    @Override
+    public void handleMessage(WsMessageContext ctx) {
+        try {
+            UserGameCommand command = new Gson().fromJson(ctx.message(), UserGameCommand.class);
+            switch (command.getCommandType()) {
+                case CONNECT -> connect(command.getAuthToken(), ctx.session);
+                case MAKE_MOVE -> makeMove(command.getAuthToken(), ctx.session);
+                case LEAVE -> leave(command.getAuthToken(), ctx.session);
+                case RESIGN -> resign(command.getAuthToken(), ctx.session);
+            }
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void handleClose(WsCloseContext ctx) {
+        System.out.println("Websocket closed");
+    }
+
+    private void connect(String authToken, Session session) throws IOException {
+//        connections.add(session);
+//        var message = String.format("%s is in the shop", visitorName);
+//        var notification = new Notification(Notification.Type.ARRIVAL, message);
+//        connections.broadcast(session, notification);
+    }
+
+    private void makeMove(String authToken, Session session) throws IOException {
+//        var message = String.format("%s left the shop", visitorName);
+//        var notification = new Notification(Notification.Type.DEPARTURE, message);
+//        connections.broadcast(session, notification);
+//        connections.remove(session);
+    }
+
+    private void leave(String authToken, Session session) {
+
+    }
+
+    private void resign(String authToken, Session session) {
+
+    }
+}
